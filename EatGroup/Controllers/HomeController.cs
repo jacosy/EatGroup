f5ddf762e626d069ba549735aefad1cef6681de9
meraruda <meraruda@gmail.com>
2016-09-25 09:50:39 +0800

    before

--- EatGroup/Controllers/HomeController.cs
+++ EatGroup/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace EatGroup.Controllers
         // GET: Default
         public ActionResult Index()
         {
-            return View();
+            return Login();
         }
 
         public ActionResult Login()
